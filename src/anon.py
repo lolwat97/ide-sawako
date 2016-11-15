ef1cdd10b90ce5ae3428362fa9cacc673159185a
Sergey Sofeychuk <lw@fmap.me>
2016-11-15 14:53:18 +0300

    anon modification idunno

--- src/anon.py
+++ src/anon.py
@@ -1,14 +1,10 @@
 import urllib.request
-import requests
 import json
 import logging
 
 def getAnonUrl(url):
     url = 'http://noblockme.ru/api/anonymize?url=' + url
-    anonString = requests.get(url).text
+    anonString = urllib.request.urlopen(url).read().decode('utf-8')
     anonJson = json.loads(anonString)
-    if anonJson['status'] == 0:
-        message = 'Анонимизировано, господин: ' + anonJson['result']
-    else:
-        message = 'А-а-а, не вижу страницы! Извините, господин! ;_;'
-    return message
+    anonUrl = anonJson['result']
+    return anonUrl
